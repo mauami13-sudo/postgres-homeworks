8b3b3be0399b3d89e7e57776d77cf6f7ca388ede
mauami13-sudo <mauami13@gmail.com>
2026-09-25 10:00:02 +0300

    Update filter_sorting.sql

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -1,14 +1,26 @@
--- Напишите запросы, которые выводят следующую информацию:
--- 1. заказы, доставленные в страны France, Germany, Spain (таблица orders, колонка ship_country)
-SELECT ...
+-- 1. Заказы, доставленные в страны France, Germany, Spain
+SELECT *
+FROM orders
+WHERE ship_country IN ('France', 'Germany', 'Spain');
 
--- 2. уникальные страны и города, куда отправлялись заказы, отсортировать по странам и городам (таблица orders, колонки ship_country, ship_city)
+-- 2. Уникальные страны и города, куда отправлялись заказы,
+--    отсортировать по странам и городам
+SELECT DISTINCT ship_country, ship_city
+FROM orders
+ORDER BY ship_country, ship_city;
 
+-- 3. Сколько дней в среднем уходит на доставку товара в Германию
+SELECT AVG(shipped_date - order_date)
+FROM orders
+WHERE ship_country = 'Germany';
 
--- 3. сколько дней в среднем уходит на доставку товара в Германию (таблица orders, колонки order_date, shipped_date, ship_country)
+-- 4. Минимальная и максимальная цена среди продуктов, не снятых с продажи
+SELECT MIN(unit_price), MAX(unit_price)
+FROM products
+WHERE discontinued != 1;
 
-
--- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи (таблица products, колонки unit_price, discontinued не равно 1)
-
-
--- 5. минимальную и максимальную цену среди продуктов, не снятых с продажи и которых имеется не меньше 20 (таблица products, колонки unit_price, units_in_stock, discontinued не равно 1)
+-- 5. Минимальная и максимальная цена среди продуктов, не снятых с продажи
+--    и которых имеется не меньше 20
+SELECT MIN(unit_price), MAX(unit_price)
+FROM products
+WHERE discontinued != 1 AND units_in_stock >= 20;
